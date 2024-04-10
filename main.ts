ee8878338e2f1e6f210b0449ff167bc677439421
HuangYiming0608 <133227329+HuangYiming0608@users.noreply.github.com>
2024-04-10 16:32:32 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,16 @@ basic.forever(function () {
     if (CrocoKit_Sensor.Ultrasonic(DigitalPin.P1, DigitalPin.P0) < 50) {
         basic.showLeds(`
             # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+        music.play(music.tonePlayable(131, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
+    }
+    if (CrocoKit_Sensor.IR(DigitalPin.P3, CrocoKit_Sensor.enObstacle.Obstacle)) {
+        basic.showLeds(`
+            # # # # #
             # # # # #
             # # # # #
             # # # # #
