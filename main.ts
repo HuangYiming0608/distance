f3bb2038873ee932901ad6220528d34af32db3ad
HuangYiming0608 <133227329+HuangYiming0608@users.noreply.github.com>
2024-04-10 16:49:47 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,7 +7,7 @@ basic.forever(function () {
             # # # # #
             # # # # #
             `)
-        music.play(music.tonePlayable(131, music.beat(BeatFraction.Breve)), music.PlaybackMode.UntilDone)
+        music.play(music.tonePlayable(988, music.beat(BeatFraction.Breve)), music.PlaybackMode.UntilDone)
     } else if (CrocoKit_Sensor.Ultrasonic(DigitalPin.P1, DigitalPin.P0) < 50) {
         if (true) {
             basic.showLeds(`
